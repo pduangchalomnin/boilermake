d4041b621751462489e2445286697a5f80479a76
marcsednaoui <marcsednaoui@hotmail.com>
2017-01-21 11:36:55 -0600

    marc7

---
+++ app/Http/Controllers/Auth/ReviewController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use App\Dine;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class ReviewCOntroller extends Controller
+{
+    public function starReview($request){
+      $review = new Review();
+      $review->customerUid = Auth::user()->auth0id;
+      $review->did=$request->did;
+      $review->star=$request->star;
+      $review->reviewText=$request->reviewText;
+
+  }
+}

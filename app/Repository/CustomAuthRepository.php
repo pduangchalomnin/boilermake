60aecbd05b385d47c897a894a76dfa784f2bedb7
patipat duangchalomnin <Patipat@pal-nat186-76-144.itap.purdue.edu>
2017-01-21 13:04:23 -0500

    created tag migration

--- app/Repository/CustomAuthRepository.php
+++ app/Repository/CustomAuthRepository.php
@@ -11,6 +11,7 @@ namespace App\Repository;
 
 use Auth0\Login\Contract\Auth0UserRepository;
 use App\User;
+use Auth0\Login\Auth0User;
 
 class CustomAuthRepository implements Auth0UserRepository
 {
@@ -26,12 +27,14 @@ class CustomAuthRepository implements Auth0UserRepository
     }
 
     public function getUserByUserInfo($userInfo) {
-        return $this->upsertUser($userInfo['profile']);
+        $upsertData = $this->upsertUser($userInfo['profile']);
+        return new Auth0User($userInfo['profile'],$userInfo['accessToken']);
     }
 
     protected function upsertUser($profile) {
         //dd($profile);
         $user = User::where("auth0id", $profile["user_id"])->first();
+
         if ($user === null) {
             // If not, create one
             $user = new User();
@@ -43,7 +46,6 @@ class CustomAuthRepository implements Auth0UserRepository
             $user->family_name = $profile["family_name"];
             $user->save();
         }
-
         return $user;
     }
 
@@ -55,7 +57,6 @@ class CustomAuthRepository implements Auth0UserRepository
 
         // build the user
         $user = $this->getUserByUserInfo($user);
-
         // it is not the same user as logged in, it is not valid
         if ($user && $user->auth0id == $identifier) {
             return $user;
